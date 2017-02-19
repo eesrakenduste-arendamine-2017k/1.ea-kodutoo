d6c98abc3509851a60f819ec6eef1fde409e4626
jantrei <jantrei@greeny.cs.tlu.ee>
2017-02-19 20:25:51 +0200

    Kodutöö

---
+++ change.js
@@ -0,0 +1,68 @@
+//Font Size\\
+function fontSmall(){
+  document.getElementById("clock").style.fontSize = "70px";
+    document.getElementById("date").style.fontSize = "30px";
+}
+
+function fontNormal(){
+  document.getElementById("clock").style.fontSize = "90px";
+  document.getElementById("date").style.fontSize = "50px";
+}
+function fontLarge(){
+  document.getElementById("clock").style.fontSize = "110px";
+  document.getElementById("date").style.fontSize = "70px";
+}
+//Background Color\\
+function backgroundGreen(){
+  document.body.style.backgroundColor = "#84BE6A";
+}
+function backgroundNavy(){
+  document.body.style.backgroundColor = "#001f3f";
+}
+function backgroundBlack(){
+  document.body.style.backgroundColor = "#232722";
+}
+function backgroundOrange(){
+  document.body.style.backgroundColor = "#FF7216";
+}
+function backgroundRed(){
+  document.body.style.backgroundColor = "#850E0E";
+}
+//Clock Color\\
+function clockGreen(){
+  document.getElementById("clock").style.color = "#008000";
+}
+function clockBlue(){
+  document.getElementById("clock").style.color = "#0000FF";
+}
+function clockBlack(){
+  document.getElementById("clock").style.color = "#000000";
+}
+function clockOrange(){
+  document.getElementById("clock").style.color = "#FFA500";
+}
+function clockRed(){
+  document.getElementById("clock").style.color = "#ff0000";
+}
+function clockOrigin(){
+  document.getElementById("clock").style.color = "#edeaea";
+}
+//Date Color\\
+function dateGreen(){
+  document.getElementById("date").style.color = "#008000";
+}
+function datekBlue(){
+  document.getElementById("date").style.color = "#0000FF";
+}
+function dateBlack(){
+  document.getElementById("date").style.color = "#000000";
+}
+function dateOrange(){
+  document.getElementById("date").style.color = "#FFA500";
+}
+function dateRed(){
+  document.getElementById("date").style.color = "#ff0000";
+}
+function dateOrigin(){
+  document.getElementById("date").style.color = "#edeaea";
+}
