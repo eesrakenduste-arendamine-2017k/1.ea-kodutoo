04e8b7f4d80cfa3e134d335fa1ba8766a19bc725
Rando Tomingas <randotm@users.noreply.github.com>
2017-02-24 14:36:41 +0200

    Timezone is changeable, but date doesn't change with it.

--- clock.js
+++ clock.js
@@ -3,109 +3,134 @@ window.onload = function() {
 	var clockContainer = document.getElementById("clock");
 	var dateContainer = document.getElementById("date");
 	var dayContainer = document.getElementById("day");
+	var timezoneContainer = document.getElementById("timezone");
+	var toTimezoneContainer = document.getElementById("toTimezone");
 	clockContainer.innerHTML = getCurrentTime();
 	dateContainer.innerHTML = getCurrentDate();
 	dayContainer.innerHTML = getCurrentDay();
+	timezoneContainer.innerHTML = getTimezone();
 
 	window.setInterval(function(){
-    	clockContainer.innerHTML = getCurrentTime();
+		clockContainer.innerHTML = getCurrentTime();
+		timezoneContainer.innerHTML = getTimezone();
+		if (getCurrentTime()=="00:00:00"){
+			dateContainer.innerHTML = getCurrentDate();
+			dayContainer.innerHTML = getCurrentDay();
+		}
 	}, 1000);
-
-	if (getCurrentTime()=="00:00:00"){
-		dateContainer.innerHTML = getCurrentDate();
-		//dateContainer.innerHTML = getCurrentDay();
-	}
-
-	window.addEventListener("keypress", function(e){
-	  	console.log(e);
-    });
 
 };
 
+var textSize = "400%";
+var clockPos = "right";
 var months = ["Jaanuar", "Veebruar", "Märts",
 	"Aprill", "Mai", "Juuni",
 	"Juuli", "August", "September",
 	"Oktoober", "November", "Detsember"];
-
 var days = ["Esmaspäev", "Teisipäev", "Kolmapäev",
-	"Neljapäev", "Reede", "Laupäev", "Pühapäev"]
+	"Neljapäev", "Reede", "Laupäev", "Pühapäev"];
+var gmtTimezone = getTimezoneOffset();
 
+function askTimezone() {
+	var timezone = prompt("Sisesta GMT ajatsoon");
+	for(i=-12; i<=12; i++) {
+		if (timezone == i) {
+			gmtTimezone = timezone;
+		}
+	}
+	return null;
+}
 
+function getDate(offset) {
+	var date = new Date();
+	if (offset != null) {
+		var milliseconds = 3600000 * offset + date.getTime();
+		date = new Date(milliseconds);
+	}
+	return date;
+}
 
-function getCurrentTime(){
-
-	var currentDate = new Date();
-
-	var hours = addZerobefore(currentDate.getHours());
-	var minutes = addZerobefore(currentDate.getMinutes());
-	var seconds = addZerobefore(currentDate.getSeconds());
-
-	var dateString = hours + ":" + minutes + ":" + seconds;
-
-	return dateString;
-};
+function getCurrentDay() {
+	var currentDate = getDate(gmtTimezone);
+	var dayOfWeek = currentDate.getUTCDay();
+	return days[dayOfWeek-1];
+}
 
 function getCurrentDate(){
-
-	var currentDate = new Date();
-
-	var month = currentDate.getMonth();
-	var year = currentDate.getYear()+1900;
-	var day = currentDate.getDate();
-
+	var currentDate = getDate(gmtTimezone);
+	var month = currentDate.getUTCMonth();
+	var year = currentDate.getUTCFullYear();
+	var day = currentDate.getUTCDate();
 	return day+" "+months[month]+" "+year;
 }
 
-var getCurrentDay = function() {
+function getCurrentTime(){
+	var currentDate = getDate(gmtTimezone);
+	var hours = addZerobefore(currentDate.getUTCHours());
+	var minutes = addZerobefore(currentDate.getUTCMinutes());
+	var seconds = addZerobefore(currentDate.getUTCSeconds());
+	var dateString = hours + ":" + minutes + ":" + seconds;
+	return dateString;
+};
 
-	var currentDate = new Date();
-
-	var dayOfWeek = currentDate.getDay();
-
-	return days[dayOfWeek-1]
+function getTimezoneOffset() {
+	var currentDate = getDate();
+	var offset = currentDate.getTimezoneOffset()/-60;
+	return offset;
 }
 
-var clockPos = "right";
+function getTimezone() {
+	var dateString = "(GMT" + checkPosVsNeg(gmtTimezone) + ")";
+	return dateString;
+}
 
 function changeClockPos(){
 	if(clockPos == "right"){
-		document.getElementById("clock").style["top"] = "0%";
-		document.getElementById("clock").style["left"] = "0%";
-		document.getElementById("clock").style["transform"] = "translate(0%, 0%)";
+		document.getElementById("time").style["top"] = "0%";
+		document.getElementById("time").style["left"] = "0%";
+		document.getElementById("time").style["transform"] = "translate(0%, 0%)";
 		clockPos = "left";
 	} else if (clockPos == "left"){
-		document.getElementById("clock").style["top"] = "0%";
-		document.getElementById("clock").style["left"] = "100%";
-		document.getElementById("clock").style["transform"] = "translate(-100%, 0%)";
+		document.getElementById("time").style["top"] = "0%";
+		document.getElementById("time").style["left"] = "100%";
+		document.getElementById("time").style["transform"] = "translate(-200%, 0%)";
 		clockPos = "right";
 	}
 	return 0;
 }
 
-var textSize = "400%";
-
 function changeTextSize(){
 	if(textSize == "400%"){
 		document.getElementById("date").style["font-size"] = "600%";
-		document.getElementById("clock").style["font-size"] = "600%";
+		document.getElementById("time").style["font-size"] = "600%";
 		document.getElementById("day").style["font-size"] = "600%";
 		textSize = "600%";
 	}else if(textSize == "600%"){
 		document.getElementById("date").style["font-size"] = "100%";
-		document.getElementById("clock").style["font-size"] = "100%";
+		document.getElementById("time").style["font-size"] = "100%";
 		document.getElementById("day").style["font-size"] = "100%";
 		textSize = "100%";
 	}else if(textSize == "100%"){
 		document.getElementById("date").style["font-size"] = "400%";
-		document.getElementById("clock").style["font-size"] = "400%";
+		document.getElementById("time").style["font-size"] = "400%";
 		document.getElementById("day").style["font-size"] = "400%";
 		textSize = "400%";
 	}
 	return 0;
 }
 
+function checkPosVsNeg(number) {
+	if (number < 0) {
+		number = "&#8722;" + addZerobefore(number*-1);
+		return number;
+	} else {
+		number = "+" + addZerobefore(number);
+		return number;
+	}
+}
+
 function addZerobefore(number) {
-	if(number < 10) {
+	if(number < 10 && number > -10) {
 		number = "0" + number;
 	}
 	return number;
