c690f0f03f45275f43b534d535663ab9cf4164da
hensav <hensav@gmail.com>
2017-03-31 08:33:29 +0300

    Kasutab Giphy apit taustapildi muutmiseks, teksti värvi on võimalik muuta.

--- js/date_time.js
+++ js/date_time.js
@@ -1,5 +1,5 @@
-function date_time(id)
-{
+function date_time(id) {
+    //"use strict";
     date = new Date;
     year = date.getFullYear();
     month = date.getMonth();
@@ -12,19 +12,42 @@ function date_time(id)
     {
         h = "0"+h;
     }
-    m = date.getMinutes();
+        m = date.getMinutes();
     if(m<10)
     {
         m = "0"+m;
     }
-    s = date.getSeconds();
+        s = date.getSeconds();
     if(s<10)
     {
         s = "0"+s;
     }
     result = ''+days[day]+'<br>'+d+'. '+months[month]+' '+year+'<br>'+h+':'+m+':'+s;
-
     document.getElementById(id).innerHTML = result;
     setTimeout('date_time("'+id+'");','1000');
     return true;
+}
+
+function XX() {
+    first2 = document.getElementById("firstname").value;
+
+    request = new XMLHttpRequest;
+    request.open('GET', 'http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag='+first2, true);
+
+    request.onload = function() {
+        if (request.status >= 200 && request.status < 400){
+            data = JSON.parse(request.responseText).data.image_url;
+            console.log(data);
+           document.querySelector(".content").style = "background-image:url("+data+")";
+
+        } else {
+            console.log('reached giphy, but API returned an error');
+        }
+    };
+
+    request.onerror = function() {
+        console.log('connection error');
+    };
+
+    request.send();
 }
