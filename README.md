cb8bd9a5f94e339077a04176112444ca52b76554
Oskar Nikopensius <nikopeos@greeny.cs.tlu.ee>
2017-04-18 23:23:47 +0300

    Added movement, dark mode, size change and name. Edited readme.

--- README.md
+++ README.md
@@ -2,43 +2,12 @@
 
 # Funktsioonid
 
-* Funktisoon 1
-* Funktisoon 2
-* Funktisoon 3
-* Funktisoon 4
-
-
+* Klikkides kogu lehe ulatuses olevale pinnale muudab taustavärvi erinevate värvide vahel
+* Vajutades klaviatuuril D klahvi, läheb üle öörežiimi
+* Klahvid S - small, M - medium(default) ja L - large muudavad kella ja kuupäeva tekstisuurust
+* Nooleklahvid parem, vasak muudavad kella positsiooni, klahv alla liigutab jälle kella tagasi keskele
 
 ---------
 
-# 1. kodutöö – Lauakell
+## Projekt asub aadressil : http://greeny.cs.tlu.ee/~nikopeos/eesrakendused/1.ea-kodutoo/
 
-Kujunda kellanäide vastavalt maitsele või kindlale teemale, mahutades kella täisekraanile, et saaks kasutada lauakella või ekraanisäästja asemel. Selleks, et see sobiks paljudele ekraanidele, kasuta kujunduse loomisel protsendilisi väärtusi (nt width: 100%; ) või võimalda kella suurust kasutajal muuta.
-
-## Nõuded
-
-1. Veebirakendus töötab, st näitab kella, kuupäeva ja nädalapäeva
-1. Vastavalt kasutaja tegevusele on võimalik vähemalt kolme lauakella atribuuti muuta (nt suurus, värv, asukoht ...)
-1. Kell on originaalne ning kasutajaliides on maitsekalt kujundatud
-1. Autori ees- ja perenimi on lehel välja toodud
-1. Veebirakendus on üles ehitatud objektorienteeritult.
-
-## Mõned ideed võimalikeks täiendusteks (lihtsalt, et mõte hakkaks jooksma)
-
-* Vahetuda võivad taustapildid, võib mängida taustal mingi muusika
-* Taustale klikkides muudetakse kella taustaväri
-* Kellale klõpsides muudetakse numbrite värvi või numbrite suurust;
-* Iga numbri suurust saab eraldi muuta
-* Nooleklahvidega saab kella ekraanil liigutada
-* Küsi enne kella näitamise alustamist kasutajalt tema lemmikvärv ja tee sellest lähtudes midagi
-* Muuda kella ja tausta värvi vastavalt ajale (päev/öö)
-* Kella suurus ja asukoht sõltuvad hiire asukohast ekraanil
-* Äkki võiks tegu olla hoopis analoogkellaga
-* Kirjatüüpi saab ka muuta [google.com/fonts](https://www.google.com/fonts)
-* Kasutaja saab lisada mitu kellaaega erinevatest maailma kohtadest
-
-## Abimaterjal
-
-* sündmuste loetelu [HTML DOM Events](http://www.w3schools.com/jsref/dom_obj_event.asp)
-* ajal põhinevad sündmused [JavaScript Timing Events](http://www.w3schools.com/js/js_timing.asp)
-* CSSi muutmine [HTML DOM Style Object](http://www.w3schools.com/jsref/dom_obj_style.asp)
--- clock.js
+++ clock.js
@@ -1,23 +1,3 @@
-function changeColor() {
-    "use strict";
-    var col = document.getElementById("changecolor");
-    col.style.backgroundColor = colors[Math.floor((Math.random()*19)+1)];
-}
-
-var colors = ["#b8b894", "#6666ff", "#b3e6cc",
-"#ffeee6", "#ff66a3", "#a6a6a6", "#cce6ff",
-"#e8e8e8", "#3333ff", "#6699cc", "#ff3333",
-"#ffff66", "#9fdfbf", "#ff471a", "#ffb84d",
-"#ff6633", "#bfff80", "#ff8080", "#e6ff99"];
-
-function checkTime(i) {
-    "use strict";
-    if (i < 10) {
-        i = "0" + i;
-    }
-    return i;
-}
-
 window.onload = function() {
     const secondP = document.getElementById('fixed');
     const text = secondP.innerText;
@@ -26,6 +6,26 @@ window.onload = function() {
     return span;
     }).join('');
     secondP.innerHTML = newText;
+}
+
+var colors = ["#b8b894", "#6666ff", "#b3e6cc",
+"#ffeee6", "#ff66a3", "#a6a6a6", "#cce6ff",
+"#e8e8e8", "#3333ff", "#6699cc", "#ff3333",
+"#ffff66", "#9fdfbf", "#ff471a", "#ffb84d",
+"#ff6633", "#bfff80", "#ff8080", "#e6ff99"];
+
+function changeColor() {
+    "use strict";
+    var col = document.getElementById("changecolor");
+    col.style.backgroundColor = colors[Math.floor((Math.random()*19)+1)];
+}
+
+function checkTime(i) {
+    "use strict";
+    if (i < 10) {
+        i = "0" + i;
+    }
+    return i;
 }
 
 function dayToName(day) {
@@ -56,7 +56,6 @@ function startTime() {
     var h = today.getHours();
     var m = today.getMinutes();
     var s = today.getSeconds();
-
     var dt = today.getDate();
     var month = today.getMonth() + 1;
     var wd = today.getDay();
@@ -70,6 +69,39 @@ function startTime() {
     var t = setTimeout(startTime, 500);
 }
 
-$("#fader").on("input",function () {
-            $('#v-28').css("font-size", $(this).val() + "px");
-    });
+window.addEventListener('keyup', function(event){
+    console.log(event.which);
+
+    if(event.which == 68){
+      document.body.style.backgroundColor ='#2a2e35';
+    }
+
+    if(event.which == 83 ){
+      document.body.style.fontSize ='0.5em';
+    }
+
+    if(event.which == 77 ){
+      document.body.style.fontSize ='1em';
+    }
+
+    if(event.which == 76 ){
+      document.body.style.fontSize ='1.5em';
+    }
+
+    if(event.which == 37 ){
+        document.getElementById("clock").style.left = '30%';
+        document.getElementById("dateAndDay").style.left = '30%';
+      }
+
+    if(event.which == 39 ){
+        document.getElementById("clock").style.left = '70%';
+        document.getElementById("dateAndDay").style.left = '70%';
+      }
+
+    if(event.which == 40 ){
+        document.getElementById("clock").style.left = '50%';
+        document.getElementById("dateAndDay").style.left = '50%';
+      }
+
+});
+
