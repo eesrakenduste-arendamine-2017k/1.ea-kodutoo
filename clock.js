a97fd9aec0db4bf0ca354d86efde72afc733b0bb
MK <mrxkollo@users.noreply.github.com>
2017-04-22 23:43:27 +0300

    Basic Clock class.

--- clock.js
+++ clock.js
@@ -1,9 +1,22 @@
+"use strict";
 function Clock() {
+
+    var addZeroBefore = function (input) {
+        if (input < 10) {
+            return "0" + input;
+        } else {
+            return input;
+        }
+    };
 
     setInterval(function () {
         var date = new Date();
         var hour = date.getHours();
         var minutes = date.getMinutes();
         var seconds = date.getSeconds();
-    })
+        console.log(addZeroBefore(hour) + ":" + addZeroBefore(minutes) + ":" + addZeroBefore(seconds));
+    }, 1000);
 }
+
+
+var clock = new Clock();
