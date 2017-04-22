393c21a57b04756615a26322bcf85162e5a3e4a8
MK <mrxkollo@users.noreply.github.com>
2017-04-23 01:07:21 +0300

    Displays full month-day, month name and weekday. Making the clock and others load at the same timing was murder....

--- clock.js
+++ clock.js
@@ -1,56 +1,68 @@
 "use strict";
-function Clock() {
+window.onload = function () {
 
-    // References to HTML.
-    var clockHTML = document.getElementById("clock-display");
-    var dateHTML = document.getElementById("date-display");
+    function Clock() {
 
-
-    // Date only returns the number, data is displayed with the lists.
-    var week_day = ["pühapäev", "esmaspäev", "teisipäev", "kolmapäev", "neljapäev", "reede", "laupäev"];
-    var months = [];
+        // References to HTML.
+        var clockHTML = document.getElementById("clock-display");
+        var dateHTML = document.getElementById("date-display");
 
 
-    // Changes the color of the clock to one of the colors in the list.
-    // When it reaches the end, it returns to the beginning of the list.
-    var colors = ["red", "blue", "purple", "black"];
-    var counter = 0;
-    clockHTML.addEventListener("click", function () {
-        clockHTML.style.color = colors[counter];
-        counter++;
-        if (counter == colors.length) {
-            counter = 0;
-        }
-    });
+        // Date only returns the number, data is displayed with the lists.
+        var week_names = ["pühapäev", "esmaspäev", "teisipäev", "kolmapäev", "neljapäev", "reede", "laupäev"];
+        var month_names = ["jaanuar", "veebruar", "märts", "aprill", "mai", "juuni", "juuli", "august", "september", "oktoober", "november", "detsember"];
 
 
-    // Adds a zero before the time.
-    var addZeroBefore = function (input) {
-        if (input < 10) {
-            return "0" + input;
-        } else {
-            return input;
-        }
-    };
+        // Changes the color of the clock to one of the colors in the list.
+        // When it reaches the end, it returns to the beginning of the list.
+        var colors = ["red", "blue", "purple", "black"];
+        var counter = 0;
+        clockHTML.addEventListener("click", function () {
+            clockHTML.style.color = colors[counter];
+            counter++;
+            if (counter == colors.length) {
+                counter = 0;
+            }
+        });
 
 
-    // Print out the time and date data.
-    setInterval(function () {
-        // Date variables.
-        var date = new Date();
-        var month_day = date.getDate();
-        var month = date.getMonth();
-        var week_day = date.getDay();
-
-        // Time variables
-        var hour = date.getHours();
-        var minutes = date.getMinutes();
-        var seconds = date.getSeconds();
-
-        clockHTML.innerHTML = (addZeroBefore(hour) + ":" + addZeroBefore(minutes) + ":" + addZeroBefore(seconds));
-        dateHTML.innerHTML = month_day + ".";
-    }, 1000);
-}
+        // Adds a zero before the time.
+        var addZeroBefore = function (input) {
+            return input < 10 ? "0" + input : input;
+        };
 
 
-var clock = new Clock();
+        // Displays time.
+        var showTime = function () {
+            // Time variables
+            var date = new Date();
+            var hour = date.getHours();
+            var minutes = date.getMinutes();
+            var seconds = date.getSeconds();
+
+            clockHTML.innerHTML = (addZeroBefore(hour) + ":" + addZeroBefore(minutes) + ":" + addZeroBefore(seconds));
+        };
+
+
+        // Displays date once.
+        (function () {
+            var date = new Date();
+            var month_day = date.getDate();
+            var month = date.getMonth();
+            var week_day = date.getDay();
+            dateHTML.innerHTML = month_day + ". " + month_names[month] + ", " + week_names[week_day] + ".";
+            showTime(); // Avoids time-delay between HTML and time showing.
+        })();
+
+
+        // Print out the time and date data.
+        setInterval(function () {
+            showTime();
+        }, 1000);
+
+    }
+
+
+    var clock = new Clock();
+};
+
