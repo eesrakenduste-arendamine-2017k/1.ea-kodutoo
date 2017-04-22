f1713131533a8ecca74dae17718231e7a5791987
MK <mrxkollo@users.noreply.github.com>
2017-04-23 00:14:47 +0300

    Basic date functionality - HTML code + variables in JS etc.

--- clock.js
+++ clock.js
@@ -1,9 +1,18 @@
 "use strict";
 function Clock() {
 
+    // References to HTML.
+    var clockHTML = document.getElementById("clock-display");
+    var dateHTML = document.getElementById("date-display");
+
+
+    // Date only returns the number, data is displayed with the lists.
+    var week_day = ["pühapäev", "esmaspäev", "teisipäev", "kolmapäev", "neljapäev", "reede", "laupäev"];
+    var months = [];
+
+
     // Changes the color of the clock to one of the colors in the list.
     // When it reaches the end, it returns to the beginning of the list.
-    var clockHTML = document.getElementById("clock-display");   // Reference to the HTML that displays the clock.
     var colors = ["red", "blue", "purple", "black"];
     var counter = 0;
     clockHTML.addEventListener("click", function () {
@@ -25,13 +34,21 @@ function Clock() {
     };
 
 
-    // Print out the time.
+    // Print out the time and date data.
     setInterval(function () {
+        // Date variables.
         var date = new Date();
+        var month_day = date.getDate();
+        var month = date.getMonth();
+        var week_day = date.getDay();
+
+        // Time variables
         var hour = date.getHours();
         var minutes = date.getMinutes();
         var seconds = date.getSeconds();
+
         clockHTML.innerHTML = (addZeroBefore(hour) + ":" + addZeroBefore(minutes) + ":" + addZeroBefore(seconds));
+        dateHTML.innerHTML = month_day + ".";
     }, 1000);
 }
 
